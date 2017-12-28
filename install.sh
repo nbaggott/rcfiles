5452ed05516f0f15bbae60091b6ecc3981117ada
Nick Baggott <nbaggott@gmail.com>
2017-12-28 12:38:44 -0800

    Add bash files

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
+set -e
+set -x
+
 MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd  )`"  # absolutized and normalized
 if [ -z "$MY_PATH"  ] ; then
 exit 1  # fail
 fi
 
-
 ln -s "${MY_PATH}/tmux.conf" ~/.tmux.conf
 ln -s "${MY_PATH}/vim/vimrc" ~/.vimrc
 ln -s "${MY_PATH}/bashrc" ~/.bashrc
+ln -s "${MY_PATH}/bash" ~/.bash
