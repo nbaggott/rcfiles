cc4f2b934c902c23f0d6236b8b929057bf0c8bc3
Nick Baggott <nbaggott@gmail.com>
2017-12-28 12:08:05 -0800

    don't clobber existing files

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@ MY_PATH="`( cd \"$MY_PATH\" && pwd  )`"  # absolutized and normalized
 if [ -z "$MY_PATH"  ] ; then
 exit 1  # fail
 fi
-ln -sf "${MY_PATH}/tmux.conf" ~/.tmux.conf
-ln -sf "${MY_PATH}/vim/vimrc" ~/.vimrc
-ln -sf "${MY_PATH}/zshrc" ~/.zshrc
+
+
+ln -s "${MY_PATH}/tmux.conf" ~/.tmux.conf
+ln -s "${MY_PATH}/vim/vimrc" ~/.vimrc
+ln -s "${MY_PATH}/bashrc" ~/.bashrc
